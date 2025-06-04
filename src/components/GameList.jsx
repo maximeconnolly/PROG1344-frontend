6e1fd4bc4a8bc968fabbeddd20af15520e566977
dj-eme <cerobichaud@icloud.com>
2025-06-04 09:16:31 -0300

    Update GameList.jsx

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch} from 'react-redux';
 import {Button, Table, Modal, Form, Input, List, Select, DatePicker} from 'antd';
 import './Lists.css'
