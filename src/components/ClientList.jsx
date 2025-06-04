25f1ca929347587402000350fcd9c91f694afb68
dj-eme <cerobichaud@icloud.com>
2025-06-04 09:08:27 -0300

    Update ClientList.jsx

--- src/components/ClientList.jsx
+++ src/components/ClientList.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import React, import {useEffect, useState} from 'react';
 import {useDispatch} from "react-redux";
 import {Form, Input, Modal, Table, Button} from 'antd';
 import {createClient, updateClient, getClients, deleteClient} from "../store/clientSlice.js";
